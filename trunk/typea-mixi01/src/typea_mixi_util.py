c5c95b2e5a1d787a5d4d5371ac022b1b343f47bf
pppiroto <pppiroto@fa5f2700-41ee-11de-b910-bdabbcd8b4c7>
2009-12-31 14:48:40 +0000

    Add Create Amazon Link Page

--- trunk/typea-mixi01/src/typea_mixi_util.py
+++ trunk/typea-mixi01/src/typea_mixi_util.py
@@ -3,6 +3,7 @@
 
 import os
 import re
+
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
@@ -10,7 +11,6 @@ from google.appengine.ext.webapp import template
 import amazon_ecs
 import urllib2
 import xml.parsers.expat
-
 
 class SearchedItem:
     ''' Amazon ItemSearch Operation の結果格納  '''
@@ -71,80 +71,12 @@ class SAXTagHandler:
                 if self.now_key == 'Title':
                     self.item_list[idx].title = data    
 
-class MainPage(webapp.RequestHandler):
-    def get(self):
-        self.redirect('/am_is?q=amazon')
-
-class AmazonItemSearch(webapp.RequestHandler):
-    ''' Amazon Product Advertising API を利用し、キーワード検索を行う
-        example http://typea-mixi01.appspot.com/am_is?q=book
-        parameters 
-            q : query keyword
-            s : view style 
-                image : image list
-                text  : text only
-                ul    : unnumbered list
-            c : keyword encoding
-    '''
-    def get(self):
-        # エンコードした値を request.GET で取得すると　KEYError ？
-        keyword = ''
-        queries = self.request.query_string.split('&')
-        for query in queries:
-            pair = query.split('=')
-            if pair[0] == 'q':
-                keyword = pair[1]
-                break;
-        try:
-            style = self.request.GET['s']  # style
-        except KeyError:
-            style = 'image'  
-        
-        try:
-            coding = self.request.GET['c']  # style
-        except KeyError:
-            coding = None 
-
-        #パラメータのデコード
-        #@see http://www.findxfine.com/default/495.html
-        #FireFox のアドレスバーに漢字を打つとUTF-8でないコードにエンコードされてしまう？
-        keyword = urllib2.unquote(keyword) #.encode('utf-8')
-        if keyword == '':
-            keyword = 'amazon'
-        if coding:
-            keyword = unicode(keyword, coding)
-       
-        item_list = self.amazon_request(keyword)
-
-        # retry 
-        if len(item_list) == 0:
-            keyword = keyword.split(' ')[0]
-            item_list = self.amazon_request(keyword)
-
-        if len(item_list) == 0:
-            m = re.match(r'(?P<kw>\w+)', keyword)
-            if m:
-                keyword = m.group('kw')
-                item_list = self.amazon_request(keyword)
-
-        if len(item_list) == 0:
-            keyword = 'Computer'
-            item_list = self.amazon_request(keyword)
-            
-        template_values = {
-            'style':style,
-            'keyword':keyword,
-            'item_list':item_list
-        }
-        
-        path = os.path.join(os.path.dirname(__file__), 'amazon_ads.html')
-        self.response.out.write(template.render(path,template_values))
-
-        
-    def amazon_request(self, keyword):
+class AmazonRequest:
+    ''' Amazon Product Advertise API　(ItemSearch) を作成'''
+    def request(self, keyword, search_index):
         operation = amazon_ecs.ItemSearch()
         operation.keywords = keyword
-        operation.search_index = 'Books'
+        operation.search_index = search_index
         operation.response_group = 'Medium'
         request = operation.request()
         
@@ -160,10 +92,123 @@ class AmazonItemSearch(webapp.RequestHandler):
         p.Parse(f.read())
 
         return h.item_list
+
+class MainPage(webapp.RequestHandler):
+    def get(self):
+        self.redirect('/am_is?q=amazon')
+
+class AmazonItemSearch(webapp.RequestHandler):
+    ''' Amazon Product Advertising API を利用し、キーワード検索を行う
+        example http://typea-mixi01.appspot.com/am_is?q=book
+        parameters 
+            q : query keyword
+            s : view style 
+                image : image list
+                text  : text only
+                ul    : unnumbered list
+                image_text : image and text
+            c : keyword encoding
+    '''
+    def get(self):
+        try:
+            keyword = self.request.GET['q']
+            keyword = keyword.encode('utf-8')
+        except:
+            keyword = 'Amazon'
+        
+        try:
+            style = self.request.GET['s']  # style
+        except:
+            style = 'image'  
+
+        try:
+            coding = self.request.GET['c']  # style
+        except:
+            coding = None 
+
+        #パラメータのデコード
+        #@see http://www.findxfine.com/default/495.html
+        #FireFox のアドレスバーに漢字を打つとUTF-8でないコードにエンコードされてしまう？
+        keyword = urllib2.unquote(keyword)
+        if keyword == '':
+            keyword = 'amazon'
+        if coding:
+            keyword = unicode(keyword, coding)
+       
+        amazon_request = AmazonRequest() 
+        search_index = 'Books'
+
+        item_list = amazon_request.request(keyword, search_index)
+
+        # retry 
+        if len(item_list) == 0:
+            keyword = keyword.split(' ')[0]
+            item_list = amazon_request.request(keyword, search_index)
+
+        if len(item_list) == 0:
+            m = re.match(r'(?P<kw>\w+)', keyword)
+            if m:
+                keyword = m.group('kw')
+                item_list = amazon_request.request(keyword, search_index)
+
+        if len(item_list) == 0:
+            keyword = 'Computer'
+            item_list = amazon_request.request(keyword, search_index)
+            
+        template_values = {
+            'style':style,
+            'keyword':keyword,
+            'item_list':item_list
+        }
+        
+        path = os.path.join(os.path.dirname(__file__), 'amazon_ads.html')
+        self.response.out.write(template.render(path,template_values))
+
+class CreateLinks(webapp.RequestHandler):
+    '''Amazon へのリンクを作成する '''
+    def post(self):
+        keyword = self.request.POST['q']
+        keyword = keyword.encode('utf-8')
+        keyword = urllib2.unquote(keyword) 
+        
+        style = self.request.POST['style']
+        search_index = self.request.POST['search_index']
+        
+        amazon_request = AmazonRequest() 
+        item_list = amazon_request.request(keyword, search_index)
+        links = ''
+        if len(item_list) > 0:
+            links_template_values = {
+                    'style':style,
+                    'keyword':keyword,
+                    'item_list':item_list
+            }
+            links_path = os.path.join(os.path.dirname(__file__), 'amazon_ads.html')
+            links =  template.render(links_path,links_template_values)
+        
+        template_values = {
+                           'keyword':keyword,
+                           'links':links,
+                           'search_index':search_index,
+                           'style':style,
+        }
+        path = os.path.join(os.path.dirname(__file__), 'create_links.html')
+        self.response.out.write(template.render(path,template_values))
+
+    def get(self):
+        template_values = {
+                           'keyword':'',
+                           'links':'',
+                           'search_index':'Books',
+                           'style':'ul',
+        }
+        path = os.path.join(os.path.dirname(__file__), 'create_links.html')
+        self.response.out.write(template.render(path,template_values))
     
 application = webapp.WSGIApplication([
-                                      ('/', MainPage),
-                                      ('/am_is', AmazonItemSearch)
+                                      ('/',             MainPage),
+                                      ('/am_is',        AmazonItemSearch),
+                                      ('/create_links', CreateLinks),
                                       ], debug=True)
 
 def main():
